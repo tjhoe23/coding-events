79a2f09d8fe01e4356d22d1d7039ded67604be20
tjhoe23 <125609687+tjhoe23@users.noreply.github.com>
2023-04-27 17:08:19 -0500

    4/27 coding-events

--- src/main/java/org/launchcode/codingevents/CodingEventsApplication.java
+++ src/main/java/org/launchcode/codingevents/CodingEventsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CodingEventsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CodingEventsApplication.class, args);
 	}
 
--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -19,19 +19,21 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
 
     // lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
     // lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
+    public String processCreateEventForm(@RequestParam String eventName) {
         events.add(eventName);
         return "redirect:";
     }
